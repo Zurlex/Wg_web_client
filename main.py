e8eaabb577da4f288564dd6c3b255144316f729a
Zurlex <arsenovdaniil6@gmail.com>
2025-07-05 17:08:22 +0400

    Add files

---
+++ main.py
@@ -0,0 +1,15 @@
+import asyncio
+from Wg_web_client.client import WireGuardWebClient
+
+
+async def main():
+    client = WireGuardWebClient("45.8.98.193:51821", "./downloads")
+
+    status = await client.get_key_status("15494993167303")
+    print(status)  # True или False
+
+    await client.disable_key("15494993167303")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
